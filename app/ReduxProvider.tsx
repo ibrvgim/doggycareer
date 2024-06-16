9ec5d8001b6099deabb5d721848c2629fbc566ed
Ibrahim <ibrv.cvrtxxn@gmail.com>
2024-06-16 18:48:12 +0200

    [fix] redux re-written

--- app/ReduxProvider.tsx
+++ app/ReduxProvider.tsx
@@ -1,10 +1,16 @@
 'use client';
 
-import store from '@/redux/store';
+import { AppStore, makeStore } from '@/redux/store';
+import { useRef } from 'react';
 import { Provider } from 'react-redux';
 
 function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
 }
 
 export default ReduxProvider;
--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import questionnaireReducer from './slices/questionnaireSlice';
 
-export default configureStore({
-  reducer: {
-    questionnaire: questionnaireReducer,
-  },
-});
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      questionnaire: questionnaireReducer,
+    },
+  });
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import questionnaireReducer from './slices/questionnaireSlice';
+
+// export default configureStore({
+//   reducer: {
+//     questionnaire: questionnaireReducer,
+//   },
+// });
